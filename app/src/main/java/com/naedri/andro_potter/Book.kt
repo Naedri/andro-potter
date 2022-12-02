56f1e8516a643562808b7e10df0b9da6b018cfa4
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-12-02 16:19:16 +0100

    feat: display books v1
    Co-authored-by: Samir Kamar <k-samir@live.fr>"

---
+++ app/src/main/java/com/naedri/andro_potter/Book.kt
@@ -0,0 +1,3 @@
+package com.naedri.andro_potter
+
+data class Book(val isbn: String, val title: String, val price: Int, val cover: String, val synopsis: Array<String>)
---
+++ app/src/main/java/com/naedri/andro_potter/BooksAdapter.kt
@@ -0,0 +1,59 @@
+package com.naedri.andro_potter
+
+import android.net.Uri
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+
+class BooksAdapter(var items : List<Book>) : RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
+
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
+
+        return BooksViewHolder(itemView)
+
+    }
+
+    override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
+        val book = items[position]
+        holder.bind(book)
+    }
+
+    override fun getItemCount() = items.size
+
+    inner class BooksViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)  {
+
+        var bookTitle: TextView
+        var bookPrice: TextView
+        var bookCover: ImageView
+        var addToCart: Button
+
+        init {
+            bookTitle = itemView.findViewById(R.id.bookTitle)
+            bookPrice = itemView.findViewById(R.id.bookPrice)
+            bookCover = itemView.findViewById(R.id.bookCover)
+
+            addToCart = itemView.findViewById(R.id.buttonAddToCart)
+
+            addToCart.setOnClickListener {
+                Log.d("BooksAdapter", bookTitle.text as String)
+            }
+        }
+
+        fun bind(book:Book){
+            bookTitle.text = book.title
+            bookPrice.text = book.price.toString() + "€"
+            bookCover.load(Uri.parse(book.cover))
+        }
+
+
+    }
+}
