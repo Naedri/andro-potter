f29c4336bcc111cb5f97e4f20c85e3c45286f8b2
JALLAIS Adrien <adrien.jallais@3ds.com>
2023-01-06 17:32:21 +0100

    fix: adding book to bucket

---
+++ app/src/main/java/com/naedri/andro_potter/model/Bucket.kt
@@ -0,0 +1,62 @@
+package com.naedri.andro_potter.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class Bucket() : Parcelable {
+    private var items: ArrayList<ItemBucket> = ArrayList()
+
+    constructor(items: ArrayList<ItemBucket>) : this() {
+        this.items = items
+    }
+
+    fun getItems(): ArrayList<ItemBucket> {
+        return items;
+    }
+
+    override fun toString(): String {
+        if (items.size>0){
+            var s = "Bucket contains \n"
+            items.map { itemBucket -> s += "- $itemBucket\n" }
+            return s
+        }
+        return "Bucket is empty."
+    }
+
+    fun addItemBucket(book: Book) {
+        var found = false
+        items.forEach {
+            if(it.book.isbn == book.isbn){
+                it.quantity = it.quantity +1
+                found = true
+            }
+        }
+        if (!found) items.add(ItemBucket(1, book))
+    }
+
+    fun removeItemBucket(book: Book) {
+        items.filter { it.book.isbn == book.isbn }.forEach {
+            if (it.quantity > 1) {
+                it.quantity = it.quantity -1
+            }
+            else items.remove(it)
+        }
+    }
+
+    fun emptyBucket() {
+        items.clear()
+    }
+
+    fun countItemBucket(): Int {
+        var acc = 0
+        items.map { itemBucket -> acc += itemBucket.quantity }
+        return acc;
+    }
+
+    fun getBucketPrice(): Int {
+        var acc = 0
+        items.map { itemBucket -> acc += itemBucket.quantity * itemBucket.book.price }
+        return acc;
+    }
+}
--- app/src/main/java/com/naedri/andro_potter/model/ItemBucket.kt
+++ app/src/main/java/com/naedri/andro_potter/model/ItemBucket.kt
@@ -1,4 +1,4 @@
-package com.naedri.andro_potter.view.bucket
+package com.naedri.andro_potter.model
 
 import android.os.Parcelable
 import com.naedri.andro_potter.model.Book
@@ -6,4 +6,7 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class ItemBucket(var quantity: Int = 1, val book: Book):Parcelable {
+    override fun toString(): String {
+        return "${quantity} x ${book.title}"
+    }
 }
